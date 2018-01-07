7553b77361ca720ff01772286f67b5f3c259e612
just10minutes <just10minutes@gmail.com>
2018-01-07 15:57:42 -0500

    v1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  title : string = 'TaxiGroupFareChange';
+  return : number = 0;
+  fare : number = 0;
+  people : number = 0;
+  total : number = 0;
 
+  constructor(public navCtrl: NavController) {    
+  }
+
+  calculate(){
+    this.return = this.total - (this.fare * this.people);
   }
 
 }
